9d8c315cc298b2d031b8d3ff72040e78c5577bbc
DavidR1997 <david.rowsellwork@gmail.com>
2018-11-06 23:24:10 +1100

    minionhealthv1

---
+++ Group Project/Assets/Scripts/AI/MinionHealth.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionHealth : MonoBehaviour
+{
+    public float minHealth;
+    public float maxHealth = 100f;
+    
+}
