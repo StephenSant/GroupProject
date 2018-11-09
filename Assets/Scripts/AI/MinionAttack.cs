14cf178b65bf45f78441a5b1a9dba6e0733967c3
DavidR1997 <david.rowsellwork@gmail.com>
2018-11-09 15:18:32 +1100

    PlayerHealth & Ai shooting v1

--- Assets/Scripts/AI/MinionAttack.cs
+++ Assets/Scripts/AI/MinionAttack.cs
@@ -5,21 +5,65 @@ using UnityEngine;
 public class MinionAttack : MonoBehaviour
 {
     public Transform muzzle;
-    public float damage = 20f;
+    public float damage = 1f;
     public LineRenderer laser;
     public float delayBetweenShots;
-    public float weaponRange = 50f;
+    public float weaponRange = 75f;
     public float distanceToTarget;
-	// Use this for initialization
-	void Start ()
+    public WaitForSeconds shotDuration = new WaitForSeconds(1f);
+    public float rateOfFire = 1f;
+    public float readyToFire;
+    // Use this for initialization
+    void Start()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        laser = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
     {
+        Vector3 origin = gameObject.transform.forward;
         RaycastHit hit;
-        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, weaponRange));
-	}
+        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, weaponRange);
+        if (hit.collider)
+        {
+            Vector3 direction = (hit.point - muzzle.position).normalized;
+            laser.SetPosition(0, muzzle.position);
+            DealDamage();
+
+        }
+    }
+    void DealDamage()
+    {
+        readyToFire = Time.time + rateOfFire;
+        StartCoroutine(Shooting());
+        Vector3 origin = gameObject.transform.forward;
+        laser.SetPosition(0, muzzle.position);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, weaponRange))
+        {
+            if (hit.collider)
+            {
+                laser.SetPosition(1, hit.point);
+                Vector3 direction = (hit.point - muzzle.position).normalized;
+                PlayerHealth pHealth = hit.collider.GetComponent<PlayerHealth>();
+                if (pHealth != null)
+                {
+                    pHealth.TakeDamage(damage);
+                }
+
+
+            }
+            else
+            {
+                laser.SetPosition(1, origin + (transform.forward * weaponRange));
+            }
+        }
+    }
+    public IEnumerator Shooting()
+    {
+        laser.enabled = true;
+        yield return shotDuration;
+        laser.enabled = false;
+    }
 }
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    public float curHealth;
+    public bool isDead;
+
+    // Use this for initialization
+    void Start()
+    {
+        curHealth = maxHealth;
+        isDead = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (curHealth >= maxHealth)
+        {
+            curHealth = maxHealth;
+        }
+        if (curHealth <= 0 && !isDead)
+        {
+            isDead = true;
+            Dead();
+        }
+
+    }
+    public void TakeDamage(float damage)
+    {
+        curHealth -= damage;
+    }
+    public void Dead()
+    {
+        
+        gameObject.SetActive(false);
+    }
+}
