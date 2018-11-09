3f8956ddb163a10bc8f6b799e0db44674d80511a
DavidR1997 <david.rowsellwork@gmail.com>
2018-11-09 12:00:12 +1100

    deletion of particle effects after shot + first iterration of ai attack

---
+++ Assets/DestroyBulletParticle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBulletParticle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //literally just destroys the empty gameobject with particle effects
+        Destroy(gameObject, 1.5f);
+	}
+}
---
+++ Assets/Scripts/AI/MinionAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionAttack : MonoBehaviour
+{
+    public Transform muzzle;
+    public float damage = 20f;
+    public LineRenderer laser;
+    public float delayBetweenShots;
+    public float weaponRange = 50f;
+    public float distanceToTarget;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RaycastHit hit;
+        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, weaponRange));
+	}
+}
