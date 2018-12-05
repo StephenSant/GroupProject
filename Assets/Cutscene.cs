772e077b9680b3efb3ff05939b23be27c68a38ed
Stephen <stephen.sant123@gmail.com>
2018-12-05 14:38:05 +1100

    Cutscene

---
+++ Assets/Cutscene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class Cutscene : MonoBehaviour
+{
+    public int gameScene;
+    public VideoPlayer videoPlayer;
+    private float wait;
+
+    // Use this for initialization
+    void Start()
+    {
+        wait = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wait--;
+        if (wait <= 0)
+        {
+            if (Input.GetKeyUp(KeyCode.Space) || !videoPlayer.isPlaying)
+            {
+                SceneManager.LoadScene(gameScene);
+            }
+        }
+    }
+}
